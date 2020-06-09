9ecb9c5d5c0c444f59f27c62991715762b138d2c
calamarbisz <60406035+calamarbisz@users.noreply.github.com>
2020-06-09 21:57:52 +0200

    Add files via upload

---
+++ sketch_pag_01.js
@@ -0,0 +1,42 @@
+var imageSprite;
+
+function preload() {}
+function windowResized() {
+  //console.log('resized');
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
+  canvas.position(0, 0);
+
+  // IMATGE PERSONATGE A L'SPRITE
+  var img = loadImage("assets/imatges/persona/persona_01.png");
+  imageSprite = createSprite(windowWidth / 2, windowHeight / 2);
+  imageSprite.addImage(img);
+}
+
+function draw() {
+  clear();
+
+  // ESCALA DE LA IMATGE (personatge)
+  // imageSprite.scale = mouseY / 800;
+  imageSprite.scale = windowHeight / 1500;
+  //imageSprite.scale = 0.1;
+
+  //CAMINAR RATOLÍ PERSONATGE
+  if (mouseX < imageSprite.position.x - 10) {
+    //flip horizontally
+    imageSprite.mirrorX(1);
+    //negative x velocity: move left
+    imageSprite.velocity.x = -1;
+  } else if (mouseX > imageSprite.position.x + 10) {
+    imageSprite.mirrorX(-1);
+    imageSprite.velocity.x = 1;
+  } else {
+    //if close to the mouse, don't move
+    imageSprite.velocity.x = 0;
+  }
+
+  drawSprites();
+}
